ad6d805964e2465fdd274c045c45e5cf84008dfc
Duong Tuan with Son <son.duongtuann@gmail.com>
2026-04-09 18:26:51 +0700

    save before switching to main

--- src/main/java/com/cinema/movie_booking/controller/MovieController.java
+++ src/main/java/com/cinema/movie_booking/controller/MovieController.java
@@ -9,7 +9,7 @@ import com.cinema.movie_booking.service.ShowtimeService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
